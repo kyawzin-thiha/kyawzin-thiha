bf12444015acea12c11dad9e41e9d3613cae0487
Kyaw Zin Thiha <github@kyawzinthiha.dev>
2024-01-08 14:39:42 +0000

    change nav bar margin

---
+++ app/src/main/java/dev/kyawzinthiha/pwa/DelegationService.java
@@ -0,0 +1,14 @@
+package dev.kyawzinthiha.pwa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
